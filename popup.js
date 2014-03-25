7359589c407323996c77cd4926a8758611f034c1
Michael <droessmj@gmail.com>
2014-03-24 19:57:10 -0500

    reworking to avoid callback errors

--- popup.js
+++ popup.js
@@ -3,56 +3,70 @@ var baseURL = 'https://uwec.courses.wisconsin.edu/';
 
 var d2lfetch = {
 
-  fetch: function() {
+  fetchHome: function() {
 
     chrome.tabs.query({currentWindow:true,active:true}, function(tab) 
     { 
       if(tab[0].url != homeURL){
-        chrome.tabs.create({url: homeURL});
-        alert("Redirected to your D2L homepage");
-      }
-    });
-
-    chrome.tabs.getSelected(null, function(tab) {
-        chrome.tabs.sendRequest(tab.id, {method: "getHTML"}, function(response) {
-            var allHTML = '';
-            if(response.method=="getHTML"){
-                allHTML = response.data;
-            }
-            
-            var element = document.createElement( 'div' );
-            element.innerHTML = allHTML;
-
-            var links = [];
-
-            $(element).find('.d2l-placeholder').find('a').each(function() {
-              //alert();
-              var content = $(this).attr('href');
-              if( content != "javascript:void(0);" && content.indexOf("ou=") != -1 ){
-                links.push($(this).attr('href'));
-              }
-            });
-
-            //foreach class link
-            $.each(links, function(key, value){
-              chrome.tabs.create({url: baseURL+value});
-            });
+        chrome.tabs.create({url: homeURL}, function(tab){
+          d2lfetch.openClasses();
         });
+
+      //if function ending
+      }else{
+        d2lfetch.openClasses();
+      }
+
     });
 
-    //a tab should be open for each active classes
-    //for each tab, navigate to content -> download
+  //end fetch
+  },
 
-  }
+  openClasses: function() { 
+    chrome.tabs.getSelected(null, function(tab) {
+      chrome.tabs.sendRequest(tab.id, {method: "getHTML"}, function(response) {
+        var allHTML = '';
+        if(response.method=="getHTML"){
+          allHTML = response.data;
+        }
+              
+        var element = document.createElement( 'div' );
+        element.innerHTML = allHTML;
 
+        var links = [];
 
+        $(element).find('.d2l-placeholder').find('a').each(function() {
+                //alert();
+          var content = $(this).attr('href');
+          if( content != "javascript:void(0);" && content.indexOf("ou=") != -1 ){
+            links.push($(this).attr('href'));
+          }
+        });
+
+        //foreach class link
+        $.each(links, function(key, value){
+          chrome.tabs.create({url: baseURL+value}, function(tab){
+                //a tab should be open for each active classes
+              //query for these tabs
+            chrome.tabs.query({url:"https://*/d2l"}, function(tab)
+            {
+              //for each tab, navigate to content -> download
+              //alert();
+              //$.each(tab, function(key, value){
+              //});
+            });
+          });
+        });
+      });
+    });
+  },
 
 };
 
 
 // Run our kitten generation script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
-  d2lfetch.fetch();
+  d2lfetch.fetchHome();
 });
 
 
